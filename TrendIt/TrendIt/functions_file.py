efb3103658103db72d8d9cf8485f585724b3c9ac
prashansa <prashansa17793@gmail.com>
2020-06-05 14:26:13 -0400

    rearranging files

---
+++ TrendIt/TrendIt/functions_file.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 14:07:49 2020
+
+@author: gupta
+"""
+
+import os 
+import pdb
+
+
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+
+import bz2
+import json
+import csv
+
+
+import glob 
+import datetime
+from datetime import datetime
+from datetime import timedelta
+# from itertools import islice, izip
+
+
+
+import collections
+from collections import Counter
+
+
+import nltk 
+# nltk.download()
+from nltk import FreqDist
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.collocations import *
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+
+
+
+import sqlite3
+
+import keras
+import re
+# import codecs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
